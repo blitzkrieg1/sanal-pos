8f45bea55df2590b27ad77198f877dd3bb3199c2
Ekrem Büyükkaya <ebuyukkaya@gmail.com>
2013-06-03 12:45:17 +0300

    Docblock

--- src/YapiKrediPOS.php
+++ src/YapiKrediPOS.php
@@ -38,6 +38,16 @@ class YapiKrediPOS implements POSInterface
             'timeOut' => 30
         );
 
+    /**
+     * Posnet nesnesinin injectionı, sanal pos bilgileri ve environment
+     * belirlemek için kullanılıyor.
+     *
+     * @param Posnet $posnet
+     * @param string $musteriID
+     * @param string $terminalID
+     * @param string $environment
+     * @return void
+     */
     public function __construct(Posnet $posnet, $musteriID, $terminalID, $environment = 'production')
     {
         // Posnet injection
@@ -49,6 +59,14 @@ class YapiKrediPOS implements POSInterface
         $this->host       = $this->hostlar[$environment];
     }
 
+    /**
+     * Kredi kartı ayarlarını yap
+     *
+     * @param string $kartNo
+     * @param string $sonKullanmaTarihi
+     * @param string $cvc
+     * @return void
+     */
     public function krediKartiAyarlari($kartNo, $sonKullanmaTarihi, $cvc)
     {
         $this->kartNo            = $kartNo;
@@ -56,22 +74,45 @@ class YapiKrediPOS implements POSInterface
         $this->cvc               = $cvc;
     }
 
+    /**
+     * Sipariş ayarlarını belirle
+     *
+     * @param decimal $tutar
+     * @param string $siparisID
+     * @return void
+     */
     public function siparisAyarlari($tutar, $siparisID)
     {
         $this->tutar     = $tutar;
         $this->siparisID = $siparisID;
     }
 
+    /**
+     * Bağlantı ayarlarını düzenle
+     *
+     * @param array $yeniAyarlar
+     * @return void
+     */
     public function baglantiAyarlari($yeniAyarlar)
     {
         $this->baglantiAyarlari = array_merge($this->baglantiAyarlari, $yeniAyarlar);
     }
 
+    /**
+     * Tüm kontrolleri yap
+     *
+     * @return bool
+     */
     public function dogrula()
     {
         return $this->krediKartiKontrolleri() and $this->siparisKontrolleri();
     }
 
+    /**
+     * Ayarları yapılan ödemeyi gerçekleştir
+     *
+     * @return YapiKrediPOSSonuc
+     */
     public function odeme()
     {
         // Kontrol yapmadan deneme yapan olabilir
@@ -104,16 +145,34 @@ class YapiKrediPOS implements POSInterface
         return new \YapiKrediPOSSonuc($this->posnet);
     }
 
+    /**
+     * Kontrol methodları
+     */
+    /**
+     * Kredi kartı için tüm kontrolleri yapan method
+     *
+     * @return bool
+     */
     protected function krediKartiKontrolleri()
     {
         return $this->kartNoKontrolleri() and $this->sonKullanmaTarihiKontrolleri() and $this->cvcKontrolleri();
     }
 
+    /**
+     * Kredi kart numarasının geçerliliğini kontrol eden method
+     *
+     * @return bool
+     */
     protected function kartNoKontrolleri()
     {
         return preg_match('/^[0-9]{16}$/', $this->kartNo);
     }
 
+    /**
+     * Kredi kart son kullanma tarihinin geçerliliğini kontrol eden method
+     *
+     * @return bool
+     */
     protected function sonKullanmaTarihiKontrolleri()
     {
         // 4 karakter ve numerik
@@ -135,21 +194,41 @@ class YapiKrediPOS implements POSInterface
         return true;
     }
 
+    /**
+     * Kredi kartı güvenlik kodunun geçerliliğini kontrol eden method
+     *
+     * @return bool
+     */
     public function cvcKontrolleri()
     {
         return preg_match('/^[0-9]{3}$/', $this->cvc);
     }
 
+    /**
+     * Sipriş için girilen tüm verileri kontrol eden method
+     *
+     * @return bool
+     */
     protected function siparisKontrolleri()
     {
         return $this->tutarKontrolleri() and $this->siparisIDKontrolleri();
     }
 
+    /**
+     * Girilen sipariş tutarını kontrol eden method
+     *
+     * @return bool
+     */
     protected function tutarKontrolleri()
     {
         return is_numeric($this->tutar) and $this->tutar > 0;
     }
 
+    /**
+     * Girilen sipariş ID'sini kontrol eden method
+     *
+     * @return bool
+     */
     protected function siparisIDKontrolleri()
     {
         return ! empty($this->siparisID);
